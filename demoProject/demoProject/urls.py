8520917f0d6a8de648e4dedcca64e854f031ce85
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-12-22 11:00:49 +0600

    added cmds

--- demoProject/demoProject/urls.py
+++ demoProject/demoProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from machine_learning import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.machine),
 ]
